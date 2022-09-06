f4698aead37af3971e381a50db38bfbee12f5e1f
d2nst <wlsdl950@gmail.com>
2022-09-07 00:37:45 +0900

    어바웃 중간완성

--- js/main.js
+++ js/main.js
@@ -22,7 +22,46 @@ mouseHover.forEach((link) => {
 // * textillate start *
 $('.tlt').textillate({
   loop: true,
-  minDisplayTime: 4000,
-  in: { effect: 'fadeInUp', delayScale: 1.2, delay: 200 },
-  out: { effect: 'fadeOutDown', delayScale: 1.2, delay: 200 },
+  in: {
+    effect: 'fadeInRight',
+    delayScale: 1.2,
+    delay: 200,
+  },
+  out: {
+    effect: 'fadeOut',
+    delay: 80,
+  },
 });
+
+// header show
+
+$(window).scroll(function () {
+  const $windowTop = $(window).scrollTop();
+  const $aboutSection = $('#about').offset().top - 100;
+  if ($windowTop > $aboutSection) {
+    $('header').fadeIn(1000).css({ display: 'block' });
+    $('#home').addClass('section1-p');
+  } else {
+    $('header').fadeOut().css({ display: 'none' });
+    $('#home').removeClass('section1-p');
+  }
+});
+
+//
+
+// h2 - title 위에서 아래로 fadein
+function tit() {
+  let scrollTop =
+    (document.documentElement.scrollTop ||
+      window.pageYOffset ||
+      window.scrollX) +
+    window.innerHeight / 2;
+
+  const sect2 = document.getElementById('about');
+  const sec2 = document.querySelector('.tit');
+
+  if (scrollTop > sect2.offsetTop) {
+    sec2.classList.add('show');
+  }
+}
+window.addEventListener('scroll', tit);
